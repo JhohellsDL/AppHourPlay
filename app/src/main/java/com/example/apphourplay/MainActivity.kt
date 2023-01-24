cef3a6b2592a3f1139663b72a0b66291d47f97ac
Jhohells Erick Dianderas Lopez <jhohellserick@gmail.com>
2023-01-24 15:16:34 -0500

    boton menubar, barra

--- app/src/main/java/com/example/apphourplay/MainActivity.kt
+++ app/src/main/java/com/example/apphourplay/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.apphourplay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.apphourplay.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var drawerLayout: DrawerLayout
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,12 +17,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navController = this.findNavController(R.id.navHostFragment)
+        drawerLayout = binding.drawerLayout
 
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.navHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
